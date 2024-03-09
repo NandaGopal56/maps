2e08ce6492a8dbd8bb4bca76dd168397f6c4db23
Nanda Gopal Pattanayak <40311776+NandaGopal56@users.noreply.github.com>
2024-03-09 18:30:56 +0530

    better error handling

--- script.js
+++ script.js
@@ -212,8 +212,20 @@ class App {
     }
 
     _onLocationPermissionDenied(error) {
-        const userMessage = `${error.message}`
-        alert(userMessage);
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                alert("User denied the request for Geolocation.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("Location information is unavailable.");
+                break;
+            case error.TIMEOUT:
+                alert("The request to get user location timed out.");
+                break;
+            default:
+                alert("An unknown error occurred.");
+                break;
+        }
     }
 
     _onHoverMovePointerToDirectionMarker(event) {
